cec3d3accd579666fe63280487acc0fb48ef3353
Rayka95 <rayka.1995@gmail.com>
2018-09-30 12:58:05 +0330

    litte array uqinge fix
    litte array uqinge fix

--- index.php
+++ index.php
@@ -1,87 +1,104 @@
 <?php
-$domain = str_replace("www.","",$_SERVER['SERVER_NAME']);
 
+include('jdf.php');
+	
+	function refreshlist(){
+		file_put_contents("todaylist.txt",null);
+		$domainlist = @file("dlist.txt");
+                $domainlist = array_unique($domainlist);
+		file_put_contents("today.txt",jdate("Y/m/d"));
+		$domaincount = count($domainlist);
+		for($i=0 ;$i<20 ;$i++){
+		$rdomain = $domainlist[mt_rand($i,$domaincount-1)];
+		file_put_contents("todaylist.txt",$rdomain,FILE_APPEND);
+		
+	}
+	
+	}
+	if(!(file_exists("today.txt"))){
+	file_put_contents("today.txt",jdate("Y/m/d"));
+	}
+	if(!(file_exists("todaylist.txt"))){
+	file_put_contents("todaylist.txt",jdate("Y/m/d"));
+	refreshlist();
+	}
+
+		$dfile = @file("today.txt");
+		$todaylist = @file("todaylist.txt");
+		
+		if ($dfile[0] != jdate("Y/m/d")){	
+		refreshlist();
+		}
+	
 ?>
+<html> 
 
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title><?php echo($_SERVER['SERVER_NAME']); ?> is For Sale</title>
-        <link href="tools/style.css" rel="stylesheet" type="text/css" media="screen" />
-        <script type="text/javascript" src="tools/jquery.min.js"></script>
-        <link href='//fonts.googleapis.com/css?family=Josefin+Sans:600,700|Damion' rel='stylesheet' type='text/css'>
-        <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta name="theme-color" content="#A9C811"/> 
+<meta name="msapplication-navbutton-color" content="#A9C811"> 
+<meta name="apple-mobile-web-app-status-bar-style" content="#A9C811"> 
+<title>GreenDomains.ir IR Domains for sale - دامنه های سبز - حراجی دامنه</title> 
+<meta name="Description" content="GreenDomains.ir IR Domains for sale - دامنه های سبز - حراجی دامنه"> 
+<meta name="Keywords" content="irdomain,greendomains,irtld,forsale,itarashe"> 
 
-    </head>
-    <body>
-    <div class="black"></div>
-    <div id="wrapper">
-        <div id="header">
-            <h1><?php echo($domain); ?></h1>
-            <h2><strong class="sep-one"></strong>For Sale<strong class="sep-two"></strong></h2>
-        </div>
-        <div id="middle">
-            <p>Contact Me <br>
-                <a href="&#x6d;&#x61;&#105;&#108;&#x74;&#x6f;&#58;&#105;&#64;&#109;&#x68;&#97;&#115;&#97;&#x6e;&#105;&#x2e;&#x69;&#x72;<?php echo("?subject=Domain%20Trading%20".$domain);?>"> Send Request To Start ! </a>
+<link rel="stylesheet" href="css/style.css"> 
+</script> 
+</head>
 
-                </span></p>
-        </div>
+<body> 
 
-    </div>
-    </body>
-    </html>
+<header> <h1><a href="/" target="_self"> Green Domains For Sale | حراجی دامنه های سبز</a></h1>
+
+<div class="topmenu"> 
+<ul> 
+<li class="currentpage"><a href="/">صفحه نخست</a></li>
+<li><a href="#">خرید دامنه</a></li>
+</ul> 
+</div></header>
+
+<section class="containter"> 
 
 
+
+<div class="clearfix">
+</div>
+
+<div id="result"> 
+<result>
+
+لیست دامنه های حراجی گرین دامین امروز<red> <?php
+
+
+ echo(jdate("Y/m/d")); ?> </red> 
+
+<article>
 <?php
-try {
-    $pop=0 ; $nop=0 ; $new=0 ;
-// staart analyzing !
-    $pdirectory = "./PopListz/";
-    $directory = "./DomainListz/";
-    $filename= ($directory.$domain.".txt");
-    $pfilename= ($pdirectory.$domain.".txt");
-    if(isset($filename)){
-        $count = file_get_contents($filename);
-        if($count > 1000) {
-            $pop =1;
-        }
-    }else{$new=1;}
-    if(isset($pfilename)){
-        $count = file_get_contents($pfilename);
-        if($count < 1000) {
-            $nop =1;
-        }
-    }else{$new=1;}
-
-    if ($new == 1){
-        $count = "1";
-        file_put_contents($filename, $count);
-    }
-    if($pop == 1){
-
-
-        $count = intval($count);
-        $count++;
-        $count = (string) $count;
-        file_put_contents($pfilename,$count);
-        if(isset($filename)){
-            unlink($filename);
-        }
-    }
-    if ($nop == 1 ){
-
-        $count++;
-        $count = (string) $count;
-        file_put_contents($filename, $count);
-
-
-    }
-}
-catch (exception $ex1)
-{
-    file_put_contents("errors.txt",$ex1."\r\n",FILE_APPEND);
-}
-
-
+$file = @file("todaylist.txt");
+                $lines = count($file); 
+                ini_set('max_execution_time', 3000);
+                for($i=0;$i<$lines;$i++){
+                    print_r("<p><a href=http://".$file[$i].">".($i+1)."- ".$file[$i]."</a>");
+                }
 ?>
+
+</article>
+
+</result>
+
+</div>
+
+<a href="#" target="_self"> <button class="submit" value="order">خرید دامنه از حراجی</button>	</a>
+<div class="clearfix">
+</div></section>
+ <footer> 
+
+<div class="footerbox">
+
+<p> Made in IRAN </p>
+<blockquote>ساخت ایران
+</blockquote> <a href="http://ITarashe.cloud" target="_blank" title="ITarashe | آی تراشه"><figure class="logo"></figure></a> 
+</div></footer> 
+</body>
+</html>
